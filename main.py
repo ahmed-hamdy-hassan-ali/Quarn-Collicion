a38da9bf9a7518e38fcbb4fb77ef9b1c86699197
ahmed-hamdy-hassan-ali <a29870365@gmail.com>
2026-05-12 19:18:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+from flet import *
+import flet_audio as fa
+import os
+
+
+
+async def main(page:Page):
+    page.scroll=ScrollMode.ALWAYS
+    def play(e:Event):
+        if any(page.services):
+            page.services.clear()
+        page.services.append(fa.Audio(
+            release_mode=fa.ReleaseMode.LOOP,
+            src=e.control.content+".mp3",autoplay=True)
+            )
+        page.update()
+    page.add(Image("shiekh.png"))
+    for file in os.listdir("src/assets"):
+        if ".mp3" not in file:
+            continue
+        page.add(TextButton(
+            os.path.splitext(file)[0],
+            width=1500,
+            style=ButtonStyle(shape=RoundedRectangleBorder()),
+            on_click=play
+            ),
+            Divider()
+            )
+    page.update()
+    
+
+run(main)
